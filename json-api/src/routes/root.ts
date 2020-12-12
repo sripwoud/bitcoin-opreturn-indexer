d76e599f9604ae1fd6fa3ca3a3d5dc6635c8e964
sripwoud <me@sripwoud.xyz>
2020-12-12 15:59:01 +0100

    Add html on root route

--- json-api/src/routes/root.ts
+++ json-api/src/routes/root.ts
@@ -2,6 +2,51 @@ import { Router } from 'express'
 
 const router = Router()
 
-router.get('/', (req, res) => res.send('Exodus Bitcoin OP_RETURN indexer'))
+router.get('/', (req, res) => {
+  const html = `
+  <!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Bitcoin OP RETURN indexing JSON API</title>
+  </head>
+  <body>
+    <div>
+      <h1>Bitcoin OP RETURN indexing JSON API</h1>
+      <h2>Routes</h2>
+      <table>
+        <tr>
+          <th>Method</th>
+          <th>Route</th>
+        </tr>
+        <tr>
+          <td>GET</td>
+          <td>/opreturn/blockheight/:blockHeight</td>
+        </tr>
+        <tr>
+          <td>GET</td>
+          <td>/opreturn/blockhash/:blockHash</td>
+        </tr>
+        <tr>
+          <td>GET</td>
+          <td>/opreturn/data/:data</td>
+        </tr>
+      </table>
+    </div>
+    <footer>
+      <p>Made by Gauthier Riou</p>
+      <a
+        rel="stylesheet"
+        target="_blank"
+        href="https://github.com/AliahChurch/GauthierTest"
+        >GitHub</a
+      >
+    </footer>
+  </body>
+</html>
+`
+  res.status(200).send(html)
+})
 
 export { router as rootRouter }
